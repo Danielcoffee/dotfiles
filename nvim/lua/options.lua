68c2e77432a0f5abc676d1d09d03ca3f4f7f33a2
Dung Tran <nhatrangvtd@gmail.com>
2026-05-21 22:41:43 +0700

    fix new config

--- nvim/lua/options.lua
+++ nvim/lua/options.lua
@@ -1,10 +1,15 @@
 vim.opt.number = true
 vim.opt.relativenumber = true
-vim.opt.scrolloff = 8
+vim.opt.scrolloff = 12
 
 vim.opt.tabstop = 4
 vim.opt.shiftwidth = 4
-vim.opt.expandtab = true
+-- vim.opt.expandtab = true
 
-vim.opt.smartindent = true
-vim.opt.cursorline = true
+-- vim.opt.smartindent = true
+-- vim.opt.cursorline = true
+
+-- copy tu vim ra window don gian hon
+vim.opt.clipboard = "unnamedplus"
+
+vim.opt.formatoptions:remove({ "r", "o", "c" }) -- tự động bỏ auto comment khi xuống dòng
--- powershell/profile.ps1
+++ powershell/profile.ps1
@@ -2,3 +2,4 @@ Set-Alias vim nvim
 Set-Alias vi nvim
 
 $env:PATH += ";D:\Daniel\dotfiles\scripts"
+$env:PATH += ";C:\Program Files\LOVE\love.exe"
